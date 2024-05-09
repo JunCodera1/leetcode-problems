65cc70048457cc0509b0baa062d086fe9c36790c
tin <minhtien0601200@gmail.com>
2024-05-09 13:50:54 +0700

    Find Pivot Index

---
+++ findPivotIndex.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        int left = 0;
+        for(int i = 0; i < nums.size(); i++){
+            right -= nums[i];
+            if(left == right) return i;
+            left += nums[i];
+        }
+        return -1;
+    }
+};
